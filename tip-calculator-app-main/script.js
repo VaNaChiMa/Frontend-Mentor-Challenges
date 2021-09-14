d31d1e2cb18410a7cf4d311a3ce028b2a62d3a62
Vadim Macovei <burebistamacovei@gmail.com>
2021-09-14 16:03:46 +0300

    #11 project changeColor
    test changeColor function

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -5,14 +5,13 @@ const tip10 = document.querySelector('#tip10');
 const tip15 = document.querySelector('#tip15');
 const tip25 = document.querySelector('#tip25');
 const tip50 = document.querySelector('#tip50');
+const tips = document.querySelectorAll('.yourRate')
 const tipCustom = document.querySelector('#custom');
 const errorMsg = document.querySelector('.people__text p');
 const setManual = document.querySelector('.setManual');
-
 const tipAmount = document.querySelector('.tipAmount__sum_display');
 const total = document.querySelector('.total__sum_display');
 const reset = document.querySelector('.reset');
-
 
 /* RESET */
 reset.addEventListener('click', function() {
@@ -24,46 +23,39 @@ reset.addEventListener('click', function() {
     nrOfPeople.classList.remove('errorBorder');
     setManual.style.display = "none";
     tipCustom.style.display = "inline";
+    setManual.value = "";
 })
 
 tip5.addEventListener('click', function() {
-    let tip = (5/100) * (Number(bill.value) / Number(nrOfPeople.value));
-    let totalBill = (Number(bill.value) / Number(nrOfPeople.value) + tip);
-    tipAmount.innerHTML = Math.round((tip + Number.EPSILON) * 100) / 100;
-    total.innerHTML = Math.round((totalBill + Number.EPSILON) * 100) / 100;
-    faceControl()
-})
+    onTipPercentChange.bind(null, 5);
+    changeColor();
+}
 
-tip10.addEventListener('click', function() {
-    let tip = (10/100) * (Number(bill.value) / Number(nrOfPeople.value));
-    let totalBill = (Number(bill.value) / Number(nrOfPeople.value) + tip);
-    tipAmount.innerHTML = Math.round((tip + Number.EPSILON) * 100) / 100;
-    total.innerHTML = Math.round((totalBill + Number.EPSILON) * 100) / 100;
-    faceControl()
-})
+);
 
-tip15.addEventListener('click', function() {
-    let tip = (15/100) * (Number(bill.value) / Number(nrOfPeople.value));
-    let totalBill = (Number(bill.value) / Number(nrOfPeople.value) + tip);
-    tipAmount.innerHTML = Math.round((tip + Number.EPSILON) * 100) / 100;
-    total.innerHTML = Math.round((totalBill + Number.EPSILON) * 100) / 100;
-    faceControl()
-})
+// tip5.addEventListener('click', () => {
+//     tip5.classList.add('activate');
+// })
+tip10.addEventListener('click', onTipPercentChange.bind(null, 10));
+tip15.addEventListener('click', onTipPercentChange.bind(null, 15));
+tip25.addEventListener('click', onTipPercentChange.bind(null, 25));
+tip50.addEventListener('click', onTipPercentChange.bind(null, 50));
 
-tip25.addEventListener('click', function() {
-    let tip = (25/100) * (Number(bill.value) / Number(nrOfPeople.value));
-    let totalBill = (Number(bill.value) / Number(nrOfPeople.value) + tip);
-    tipAmount.innerHTML = Math.round((tip + Number.EPSILON) * 100) / 100;
-    total.innerHTML = Math.round((totalBill + Number.EPSILON) * 100) / 100;
-    faceControl()
-})
+function changeColor() {
+    tips.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            e.currentTarget.style.backgroundColor = "#9fe8df";
+            e.currentTarget.style.color = "hsl(183, 100%, 15%)";
+            setTimeout(() => {
+                item.currentTarget.style.backgroundColor = 'hsl(183, 100%, 15%)'
+                item.currentTarget.style.color = 'white'
+            }, 2000) 
+        })
+    })
+}
 
-tip50.addEventListener('click', function() {
-    let tip = (50/100) * (Number(bill.value) / Number(nrOfPeople.value));
-    let totalBill = (Number(bill.value) / Number(nrOfPeople.value) + tip);
-    tipAmount.innerHTML = Math.round((tip + Number.EPSILON) * 100) / 100;
-    total.innerHTML = Math.round((totalBill + Number.EPSILON) * 100) / 100;
-    faceControl()
+setManual.addEventListener('input', function() {
+    onTipPercentChange(setManual.value);
 })
 
 function faceControl() {
@@ -85,10 +77,24 @@ tipCustom.addEventListener('click', function() {
     setManual.style.display = "inline";
 })
 
-setManual.addEventListener('input', function() {
-    let tip = (Number(setManual.value)/100) * (Number(bill.value) / Number(nrOfPeople.value));
-    let totalBill = (Number(bill.value) / Number(nrOfPeople.value) + tip);
-    tipAmount.innerHTML = Math.round((tip + Number.EPSILON) * 100) / 100;
-    total.innerHTML = Math.round((totalBill + Number.EPSILON) * 100) / 100;
-    faceControl()
-})
+function onTipPercentChange(tipPercent) {
+    const calc = calcTipAmount(tipPercent, bill.value, nrOfPeople.value);
+    tipAmount.innerHTML = calc.tipAmount;
+    total.innerHTML = calc.total;
+    faceControl();
+}
+
+function calcTipAmount(tipPercent, bill, nrOfPeople) {
+    bill = Number(bill);
+    nrOfPeople = Number(nrOfPeople);
+    let tip = (tipPercent / 100) * (bill / nrOfPeople);
+    let totalBill = (bill / nrOfPeople + tip);
+    return {
+        tipAmount: round100(tip),
+        total: round100(totalBill)
+    };
+}
+
+function round100(num) {
+    return Math.round((num + Number.EPSILON) * 100) / 100;
+}

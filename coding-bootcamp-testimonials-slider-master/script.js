0d9d92fcff4ccc095898a0916b23384e59d23625
Vadim Macovei <burebistamacovei@gmail.com>
2021-09-27 22:47:42 +0300

    #15 Project
    Added mobile version
    Fixed JS part

--- coding-bootcamp-testimonials-slider-master/script.js
+++ coding-bootcamp-testimonials-slider-master/script.js
@@ -6,30 +6,33 @@ const slide_2 = document.querySelector('.slider_2');
 const prev = document.querySelector('.left-arrow');
 const next = document.querySelector('.right-arrow');
 
-let counter = 0;
-console.log(slides)
-
+let counter = 1;
 
 next.addEventListener("click", function () {
-  // slide_2.classList.add('active');
-  // slide_1.style.display = "none";
-
   counter = counter + 1;
 
-  if (counter > slides.length) {
+  if (counter > slides.length - 1) {
     counter = 0;
+    slide_2.classList.add('active');
+    slide_1.style.display = "none";
+  } else {
+    slide_2.classList.remove('active');
+    slide_1.style.display = "flex";
   }
 
-  console.log(counter);
 });
 
 prev.addEventListener("click", function () {
-  // slide_2.classList.add('active');
-  // slide_1.style.display = "none";
-
+  
   counter = counter - 1;
 
-  if (counter < 0) {
+  if (counter < 1) {
     counter = slides.length;
+    slide_2.classList.add('active');
+    slide_1.style.display = "none";
+  } else {
+    slide_2.classList.remove('active');
+    slide_1.style.display = "flex";
   }
+  
 });

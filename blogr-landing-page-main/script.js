01fb0cfa93a6cbe6df39529e70440ff8e84e5399
Vadim Macovei <burebistamacovei@gmail.com>
2022-07-20 16:05:43 +0300

    Final update for blogr project
    Small improvment:
    mobile navigation
    fix UI sections

--- blogr-landing-page-main/script.js
+++ blogr-landing-page-main/script.js
@@ -4,6 +4,8 @@ const navigation = document.querySelector(".links");
 const buttons = document.querySelectorAll(".dropbtn");
 const navArrow = document.querySelectorAll(".arrow");
 
+/* Mobile Navigation */
+
 hamburger.addEventListener("click", () => {
   navigation.classList.toggle("active");
   hamburger.classList.toggle("active");
@@ -20,12 +22,5 @@ for (let i = 0; i < buttons.length; i++) {
     sibling.classList.toggle("active");
     child.classList.toggle("active");
     bb.classList.toggle("active");
-    // btnName.classList.toggle("active");
-
-    // if (this.firstElementChild.innerText === "+") {
-    //   this.firstElementChild.innerText = "-";
-    // } else {
-    //   this.firstElementChild.innerText = "+";
-    // }
   });
 }

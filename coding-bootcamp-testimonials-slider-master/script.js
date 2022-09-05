1083cbf0d9f4d2bd22f2097812275d46325e46f6
Vadim Macovei <burebistamacovei@gmail.com>
2022-09-05 23:59:27 +0300

    Update Coding bootcamp project
    Improve position of arrows

--- coding-bootcamp-testimonials-slider-master/script.js
+++ coding-bootcamp-testimonials-slider-master/script.js
@@ -1,38 +1,39 @@
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll(".slide");
 
-const slide_1 = document.querySelector('.slider_1');
-const slide_2 = document.querySelector('.slider_2');
+const slide_1 = document.querySelector(".slider_1");
+const slide_2 = document.querySelector(".slider_2");
 
-const prev = document.querySelector('.left-arrow');
-const next = document.querySelector('.right-arrow');
+const prev = document.querySelectorAll(".left-arrow");
+const next = document.querySelectorAll(".right-arrow");
 
 let counter = 1;
 
-next.addEventListener("click", function () {
-  counter = counter + 1;
-
-  if (counter > slides.length - 1) {
-    counter = 0;
-    slide_2.classList.add('active');
-    slide_1.style.display = "none";
-  } else {
-    slide_2.classList.remove('active');
-    slide_1.style.display = "flex";
-  }
-
+next.forEach(function (item) {
+  item.addEventListener("click", function () {
+    counter = counter + 1;
+    console.log(`next ${counter}`);
+    if (counter > slides.length - 1) {
+      counter = 0;
+      slide_2.classList.add("active");
+      slide_1.style.display = "none";
+    } else {
+      slide_2.classList.remove("active");
+      slide_1.style.display = "flex";
+    }
+  });
 });
 
-prev.addEventListener("click", function () {
-  
-  counter = counter - 1;
-
-  if (counter < 1) {
-    counter = slides.length;
-    slide_2.classList.add('active');
-    slide_1.style.display = "none";
-  } else {
-    slide_2.classList.remove('active');
-    slide_1.style.display = "flex";
-  }
-  
+prev.forEach(function (item) {
+  item.addEventListener("click", function () {
+    counter = counter - 1;
+    console.log(`prev ${counter}`);
+    if (counter < 1) {
+      counter = slides.length;
+      slide_2.classList.add("active");
+      slide_1.style.display = "none";
+    } else {
+      slide_2.classList.remove("active");
+      slide_1.style.display = "flex";
+    }
+  });
 });

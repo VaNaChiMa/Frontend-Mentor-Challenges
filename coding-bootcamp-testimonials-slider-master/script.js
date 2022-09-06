443184e5d450c781b71d190301744e77077386f5
Vadim Macovei <burebistamacovei@gmail.com>
2022-09-06 03:16:02 +0300

    Update Coding bootcamp project
    Reformatted whole project

--- coding-bootcamp-testimonials-slider-master/script.js
+++ coding-bootcamp-testimonials-slider-master/script.js
@@ -1,39 +1,59 @@
-const slides = document.querySelectorAll(".slide");
+const developers = [
+  {
+    name: "Tanya Sinclair",
+    role: "UX Engineer",
+    imgUrl: "images/image-tanya.jpg",
+    text: "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+  },
+  {
+    name: "John Tarkpor",
+    role: "Junior Front-end Developer",
+    imgUrl: "images/image-john.jpg",
+    text: "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+  },
+];
 
-const slide_1 = document.querySelector(".slider_1");
-const slide_2 = document.querySelector(".slider_2");
+const sliderImg = document.querySelector("slider__image");
+const image = document.querySelector(".image");
+const slideText = document.querySelector(".slideText");
+const slideName = document.querySelector(".slideName");
+const slideRole = document.querySelector(".slideRole");
 
-const prev = document.querySelectorAll(".left-arrow");
-const next = document.querySelectorAll(".right-arrow");
+const prev = document.querySelector(".left-arrow");
+const next = document.querySelector(".right-arrow");
 
-let counter = 1;
+let counter = 0;
 
-next.forEach(function (item) {
-  item.addEventListener("click", function () {
-    counter = counter + 1;
-    console.log(`next ${counter}`);
-    if (counter > slides.length - 1) {
-      counter = 0;
-      slide_2.classList.add("active");
-      slide_1.style.display = "none";
-    } else {
-      slide_2.classList.remove("active");
-      slide_1.style.display = "flex";
-    }
-  });
+next.addEventListener("click", function () {
+  counter = counter + 1;
+  if (counter > developers.length - 1) {
+    counter = 0;
+    image.src = developers[counter].imgUrl;
+    slideName.textContent = developers[counter].name;
+    slideRole.textContent = developers[counter].role;
+    slideText.textContent = `“ ${developers[counter].text} “`;
+  } else {
+    image.src = developers[counter].imgUrl;
+    slideName.textContent = developers[counter].name;
+    slideRole.textContent = developers[counter].role;
+    slideText.textContent = `“ ${developers[counter].text} “`;
+  }
+  // console.log(`next ${counter}`);
 });
 
-prev.forEach(function (item) {
-  item.addEventListener("click", function () {
-    counter = counter - 1;
-    console.log(`prev ${counter}`);
-    if (counter < 1) {
-      counter = slides.length;
-      slide_2.classList.add("active");
-      slide_1.style.display = "none";
-    } else {
-      slide_2.classList.remove("active");
-      slide_1.style.display = "flex";
-    }
-  });
+prev.addEventListener("click", function () {
+  counter = counter + 1;
+  if (counter > developers.length - 1) {
+    counter = 0;
+    image.src = developers[counter].imgUrl;
+    slideName.textContent = developers[counter].name;
+    slideRole.textContent = developers[counter].role;
+    slideText.textContent = `“ ${developers[counter].text} “`;
+  } else {
+    image.src = developers[counter].imgUrl;
+    slideName.textContent = developers[counter].name;
+    slideRole.textContent = developers[counter].role;
+    slideText.textContent = `“ ${developers[counter].text} “`;
+  }
+  // console.log(`prev ${counter}`);
 });
